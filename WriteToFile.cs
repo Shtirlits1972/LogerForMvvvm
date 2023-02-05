4a6917cfa63fe4e528f967bb81d3636e93fb5427
Shtirlits1972 <shtirlits1972@gmail.com>
2023-02-05 02:14:52 +0200

    initial commit
    initial commit

---
+++ WriteToFile.cs
@@ -0,0 +1,12 @@
+﻿namespace LoggerClass
+{
+    public class WriteToFile : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            string baseDir = Environment.CurrentDirectory;
+            string data = DateTime.Now.ToString("G");
+            File.AppendAllText(baseDir + "/Log.txt", data + ":" + message + "\r\n");
+        }
+    }
+}
